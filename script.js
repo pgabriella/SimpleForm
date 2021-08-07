78328785a4cffb8b56d5ea6b6d79d4a05bd9c538
pgabriella <81381710+pgabriella@users.noreply.github.com>
2021-08-07 22:24:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function myFunction () {
+    document.getElementById("btn").onclick =
+    location.href = "https://www.facebook.com/";
+    
+};
+function myFunc () {
+    document.getElementById("btn1").onclick =
+    location.href = "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww.linkedin.com%2Ffeed%2F";
+};
+
+function myFuncTwo(){
+    document.getElementById("btn2").onclick=
+location.href = "https://www.instagram.com/";
+}
+function myFuncThree(){
+    document.getElementById("btn3").onclick=
+    location.href = "https://twitter.com/";
+};
+
+function login(){
+    alert("you logged in successfully!")
+
+}
